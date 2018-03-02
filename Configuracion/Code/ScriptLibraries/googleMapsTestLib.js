55954beb2e2899e99b3938b2eb4a4967866cb6dc
Fernando <fernandor@applinet.com.ar>
2018-03-02 08:57:32 -0300

    Muevo a nuevo repo

--- Configuracion/Code/ScriptLibraries/googleMapsTestLib.js
+++
@@ -1,7 +0,0 @@
-	var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.601035, lng: -58.45266400000003},
-          zoom: 20
-        });
-      }
