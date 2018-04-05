95de69806f610f4da9619fa10a41457e76fca286
Fernando <fernandor@applinet.com.ar>
2018-04-05 15:10:53 -0300

    Proveeores vista, formulario y bean

---
+++ L8669/Code/Java/ar/com/ada3d/controller/ProveedorBean.java
@@ -0,0 +1,172 @@
+package ar.com.ada3d.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import javax.faces.model.SelectItem;
+import org.openntf.domino.Document;
+import lotus.domino.NotesException;
+import ar.com.ada3d.connect.QueryAS400;
+import ar.com.ada3d.model.Proveedor;
+import ar.com.ada3d.utilidades.DocUsr;
+import ar.com.ada3d.utilidades.JSFUtil;
+
+
+public class ProveedorBean implements Serializable{
+	private static final long serialVersionUID = 1L;
+	public List<Proveedor> listaProveedores;
+	private Proveedor proveedor;
+	private String proveedorSelected = "";
+	
+	
+	public ProveedorBean() {
+		// Empty constructor
+	}
+
+	/**Cuando presiona btnNewProveedor
+	 * Creo un objeto vacio
+	 */
+	public void createNewProveedor() {
+		setProveedor(new Proveedor());
+	}
+	
+	
+	/**Cuando presiona btnSave Proveedor
+	 */
+	public ArrayList<String> saveProveedor() {
+		ArrayList<String> listAcumulaErroresAS400 = new ArrayList<String>();
+		try {
+			//TODO: Validar datos del proveedor. Duplicidad
+			Document docDummy = JSFUtil.getDocDummy();
+			docDummy.appendItemValue("razon", proveedor.getPrv_razon());
+			docDummy.appendItemValue("domici", proveedor.getPrv_domici());
+			docDummy.appendItemValue("locali", proveedor.getPrv_locali());
+			docDummy.appendItemValue("cuit", proveedor.getPrv_cuit().replaceAll("-", ""));
+			docDummy.appendItemValue("telef", proveedor.getPrv_telef());
+			
+			QueryAS400 query = new QueryAS400();
+			DocUsr docUsuario = (DocUsr) JSFUtil.resolveVariable("DocUsr");
+			String errCode = ar.com.ada3d.utilidades.Conversores.DateToString(Calendar.getInstance().getTime(), docUsuario.getUserSec() + "ddMMyyHHmmss" );
+			docDummy.appendItemValue("codadm", docUsuario.getUserDB());
+			
+			if (!query.updateAS("updateProveedorPROVE", docDummy)) {
+				if (!query.updateAS("updateProveedorPROVE_insert", docDummy)){				
+					listAcumulaErroresAS400.add("btnSave~Por favor comuniquese con Sistemas Administrativos e informe el código de error: " + errCode);
+					System.out.println("ERROR: " + errCode + " METH:saveProveedor" + "_CUIT:" + proveedor.getPrv_cuit() + "_DESC: No se pudo actualizar la tabla PH_PROVE.");
+				}
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return listAcumulaErroresAS400;
+	}
+	
+	
+	/**Cuando ingresa a la vista de Proveedores
+	 */
+	public void viewProveedores(){
+		fillListaProveedores();
+	}
+	
+	
+	/**
+	 * Lleno listaProveedores consultando As400, cada linea separa el dato por un pipe
+	 */
+	private void fillListaProveedores(){
+		ArrayList<String> nl = null;
+		QueryAS400 query = new ar.com.ada3d.connect.QueryAS400();
+		try {
+			nl = query.getSelectAS("controllerProveedores", null, false);
+		} catch (NotesException e) {
+			e.printStackTrace();
+		}
+		listaProveedores = new ArrayList<Proveedor>();
+		for (String strLinea : nl) {
+			Proveedor myProveedor = new Proveedor();
+			myProveedor.setPrv_razon(strLinea.split("\\|")[0].trim());
+			myProveedor.setPrv_domici(strLinea.split("\\|")[1].trim());
+			myProveedor.setPrv_locali(strLinea.split("\\|")[2].trim());
+			myProveedor.setPrv_cuit(strLinea.split("\\|")[3].trim());
+			listaProveedores.add(myProveedor);
+		}
+	}
+
+	
+	/**
+	 * Esto devuelve para cada usuario el ComboBox de Proveedores
+	 * @return: etiqueta y valor para xp:comboBox
+	 * @usedIn: Combo principal en ccLayoutBootstrap, está asociado a una
+	 * sessionScope(edificioWork)
+	 */
+	public List<SelectItem> getComboboxMyProveedores() {
+		if(listaProveedores == null)
+			fillListaProveedores();
+		List<SelectItem> options = new ArrayList<SelectItem>();
+		for (Proveedor myProveedor : listaProveedores) {
+			if (options.isEmpty() && proveedorSelected.equals("") ) //Primer pasada
+				proveedorSelected = myProveedor.getPrv_cuit();
+			SelectItem option = new SelectItem();
+			
+			option.setLabel(myProveedor.getPrv_razon());
+			option.setValue(myProveedor.getPrv_cuit());
+			options.add(option);
+		}
+		return options;
+	}
+
+
+
+	/**
+	 * Devuelve un string con la lista de proveedores para el cc:TypeAhead
+	 *  @param verdadero devuelve solo los que trabaja el usuario, sino todos
+	 *  @return un string con la lista de proveedores
+	 *  @usedin typeAhead
+	 * */
+		
+	public ArrayList<String> getArraySrtringListaProveedores(boolean prm_isDeTrabajo) {
+		ArrayList<String> result = new ArrayList<String>();
+		if (listaProveedores == null)
+			fillListaProveedores();
+		List<Proveedor> lista = prm_isDeTrabajo ? listaProveedores : listaProveedores;
+		for (Proveedor myProveedor : lista) {
+			result.add(myProveedor.getPrv_razon() + "|" + myProveedor.getPrv_domici() +"|" + myProveedor.getPrv_cuit());
+		}
+		return result;
+	}
+	
+
+	//Getters & Setters
+	public List<Proveedor> getListaProveedores() {
+		return listaProveedores;
+	}
+	
+	public Proveedor getProveedorPorCuit(String cuit) {
+		if (listaProveedores == null)
+			fillListaProveedores();
+		for (Proveedor myProveedor : listaProveedores){
+			if (myProveedor.getPrv_cuit().equals(cuit)){
+				return myProveedor;				
+			}
+		}
+		return proveedor;
+	}
+	
+	public Proveedor getProveedor() {
+		return proveedor;
+	}
+
+	public void setProveedor(Proveedor proveedor) {
+		this.proveedor = proveedor;
+	}
+
+	public String getProveedorSelected() {
+		return proveedorSelected;
+	}
+
+	public void setProveedorSelected(String proveedorSelected) {
+		this.proveedorSelected = proveedorSelected;
+	}
+	
+}
---
+++ L8669/Code/Java/ar/com/ada3d/model/Proveedor.java
@@ -0,0 +1,93 @@
+package ar.com.ada3d.model;
+
+
+public class Proveedor {
+	private String prv_razon;
+	private String prv_domici;
+	private String prv_locali;
+	private String prv_cuit;
+	private String prv_telef;
+	private String prv_tipoFa;
+	private String prv_estado;
+	private String prv_codadm;
+	private String prv_contac;
+	
+	public Proveedor() {
+		// Empty constructor
+	}
+
+	//Getters & Setters
+	
+	public String getPrv_razon() {
+		return prv_razon;
+	}
+
+	public void setPrv_razon(String prv_razon) {
+		this.prv_razon = prv_razon;
+	}
+
+	public String getPrv_domici() {
+		return prv_domici;
+	}
+
+	public void setPrv_domici(String prv_domici) {
+		this.prv_domici = prv_domici;
+	}
+
+	public String getPrv_locali() {
+		return prv_locali;
+	}
+
+	public void setPrv_locali(String prv_locali) {
+		this.prv_locali = prv_locali;
+	}
+
+	public String getPrv_cuit() {
+		return prv_cuit;
+	}
+
+	public void setPrv_cuit(String prv_cuit) {
+		this.prv_cuit = prv_cuit;
+	}
+
+	public String getPrv_telef() {
+		return prv_telef;
+	}
+
+	public void setPrv_telef(String prv_telef) {
+		this.prv_telef = prv_telef;
+	}
+
+	public String getPrv_tipoFa() {
+		return prv_tipoFa;
+	}
+
+	public void setPrv_tipoFa(String prv_tipoFa) {
+		this.prv_tipoFa = prv_tipoFa;
+	}
+
+	public String getPrv_estado() {
+		return prv_estado;
+	}
+
+	public void setPrv_estado(String prv_estado) {
+		this.prv_estado = prv_estado;
+	}
+
+	public String getPrv_codadm() {
+		return prv_codadm;
+	}
+
+	public void setPrv_codadm(String prv_codadm) {
+		this.prv_codadm = prv_codadm;
+	}
+
+	public String getPrv_contac() {
+		return prv_contac;
+	}
+
+	public void setPrv_contac(String prv_contac) {
+		this.prv_contac = prv_contac;
+	}
+	
+}
