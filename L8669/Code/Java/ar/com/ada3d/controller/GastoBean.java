df51ac66173df139ef693e2a6033b2992a98bf45
Fernando <fernandor@applinet.com.ar>
2018-05-31 18:16:21 -0300

    Init gastos

---
+++ L8669/Code/Java/ar/com/ada3d/controller/GastoBean.java
@@ -0,0 +1,114 @@
+package ar.com.ada3d.controller;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+import org.openntf.domino.Document;
+
+import lotus.domino.NotesException;
+import ar.com.ada3d.connect.QueryAS400;
+import ar.com.ada3d.model.Gasto;
+import ar.com.ada3d.utilidades.JSFUtil;
+
+public class GastoBean {
+	private static final long serialVersionUID = 1L;
+	public List<Gasto> listaGastos;
+	private Gasto gasto;
+	
+	public GastoBean() {
+		// Empty constructor
+	}
+	
+	/**Cuando presiona btnNewGasto
+	 * Creo un objeto vacio
+	 */
+	public void createNewGasto() {
+		setGasto(new Gasto());
+	}
+	
+	public void editFormulario(){
+		this.gasto.setIsReadMode(false);
+	}
+	
+	/**Cuando presiona btnSave Gasto
+	 */
+	public ArrayList<String> saveGasto() {
+		return null;
+		
+	}
+	
+	/**Cuando ingresa a la vista de Gastos
+	 */
+	public void viewGastos(){
+		fillListaGastos();
+	}
+	
+	/**
+	 * Lleno listaGastos consultando As400, cada linea separa el dato por un pipe
+	 * Cada gasto puede tener mas de una linea, pero existe un unico importe por factura
+	 */
+	private void fillListaGastos(){
+		ArrayList<String> nl = null;
+		QueryAS400 query = new ar.com.ada3d.connect.QueryAS400();
+		try {
+			nl = query.getSelectAS("controllerGastos", null);
+		} catch (NotesException e) {
+			e.printStackTrace();
+		}
+		listaGastos = new ArrayList<Gasto>();
+		for (String strLinea : nl) {
+			Gasto myGasto = new Gasto();
+			myGasto.setCodigoEdificio(strLinea.split("\\|")[0].trim());
+			myGasto.setNumeroComprobante(new BigDecimal(ar.com.ada3d.utilidades.Conversores.stringToStringDecimal(strLinea.split("\\|")[1].trim(), Locale.UK, 0)));
+			myGasto.setImporte(new BigDecimal(ar.com.ada3d.utilidades.Conversores.stringToStringDecimal(strLinea.split("\\|")[2].trim(), Locale.UK, 2)));
+			myGasto.setCuitProveedor(strLinea.split("\\|")[3].trim());
+			myGasto.setIdGasto(strLinea.split("\\|")[4].trim());
+			myGasto.setIsReadMode(true);
+			listaGastos.add(myGasto);
+		}
+	}
+
+	private void updateGasto(Gasto myGasto){
+		ArrayList<String> nl = null;
+		Document docDummy = JSFUtil.getDocDummy();
+		docDummy.appendItemValue("Codigo", myGasto.getCodigoEdificio());
+		docDummy.appendItemValue("Cuit", myGasto.getCuitProveedor());
+		docDummy.appendItemValue("Idgasto", myGasto.getIdGasto());
+		QueryAS400 query = new ar.com.ada3d.connect.QueryAS400();
+		try {
+			nl = query.getSelectAS("controllerUnGasto", docDummy);
+		} catch (NotesException e) {
+			e.printStackTrace();
+		}
+		Vector<String> arrTextos = new Vector<String>();
+		for (String strLinea : nl) {
+			arrTextos.add(strLinea.split("\\|")[0].trim());
+		}
+		myGasto.setTextoDetalleFactura(arrTextos);
+		myGasto.setCuitProveedor("24036435");
+	}
+
+
+	//Getters & Setters
+	
+	public List<Gasto> getListaGastos() {
+		return listaGastos;
+	}
+	
+	public Gasto getGasto() {
+		return gasto;
+	}
+
+
+	public void setGasto(Gasto gasto) {
+		this.gasto = gasto;
+		
+	}
+
+	public void setGasto(Gasto gasto, boolean updateAS400) {
+		System.out.println("Setter del gasto con update");
+		updateGasto(gasto);
+		this.gasto = gasto;
+	}
+	
+}
--- L8669/Code/Java/ar/com/ada3d/model/Gasto.java
+++ L8669/Code/Java/ar/com/ada3d/model/Gasto.java
@@ -1,11 +1,315 @@
 package ar.com.ada3d.model;
 
-public class Gasto {
+import java.math.BigDecimal;
+import java.util.*;
 
+public class Gasto {
+	static final String codigoRegistro = "G";
+	private String codigoEdificio;
+	
+	private Date fechaFactura;
+	private BigDecimal numeroComprobante; 
+	private BigDecimal numeroFactura;
+	private Vector<String> textoDetalleFactura;
+	private BigDecimal importe;
+	private Integer numeroRenglon;
+	private Integer cantidadRenglones;
+	
+	private String posicionPorcentual; //limitado a los porcentuales del edificio Porcentual 1,2,3,4 %
+	private String agrupamiento; //o rubro --> tabla
+	private String codigoEspecial; // --> tabla (es el estado)
+	private String cuitProveedor;
+	
+	private Date fechaLiquidacion; //MMAAAA
+	private String estadoDadoDeBaja; // es una 'B'
+	private String menuOrigenDatos;
+	private Date fechaCreacion;
+	private String userSequentialCreacion;
+	private Date fechaUltimaModificacion;
+	private String userSequentialUltimaModificacion;
+	
+	private Date fechaOdp;
+	private Integer numeroOdp;
+	
+	private Date fechaCajaDiaria;
+	private Integer numeroCajaDiaria;
+	
+	private String idGasto; // NCTROL numero interno de control
+
+	private boolean isReadMode;
+	private String lockedBy;
+	
+	
 	public Gasto() {
 		// Empty constructor
 	}
 
-	
 
+	//Getters & Setters
+	
+	public String getCodigoEdificio() {
+		return codigoEdificio;
+	}
+
+
+	public void setCodigoEdificio(String codigoEdificio) {
+		this.codigoEdificio = codigoEdificio;
+	}
+
+
+	public Date getFechaFactura() {
+		return fechaFactura;
+	}
+
+
+	public void setFechaFactura(Date fechaFactura) {
+		this.fechaFactura = fechaFactura;
+	}
+
+
+	public BigDecimal getNumeroComprobante() {
+		return numeroComprobante;
+	}
+
+
+	public void setNumeroComprobante(BigDecimal numeroComprobante) {
+		this.numeroComprobante = numeroComprobante;
+	}
+
+
+	public BigDecimal getNumeroFactura() {
+		return numeroFactura;
+	}
+
+
+	public void setNumeroFactura(BigDecimal numeroFactura) {
+		this.numeroFactura = numeroFactura;
+	}
+
+
+	public Vector<String> getTextoDetalleFactura() {
+		return textoDetalleFactura;
+	}
+
+
+	public void setTextoDetalleFactura(Vector<String> textoDetalleFactura) {
+		this.textoDetalleFactura = textoDetalleFactura;
+	}
+
+
+	public BigDecimal getImporte() {
+		return importe;
+	}
+
+
+	public void setImporte(BigDecimal importe) {
+		this.importe = importe;
+	}
+
+
+	public Integer getNumeroRenglon() {
+		return numeroRenglon;
+	}
+
+
+	public void setNumeroRenglon(Integer numeroRenglon) {
+		this.numeroRenglon = numeroRenglon;
+	}
+
+
+	public Integer getCantidadRenglones() {
+		return cantidadRenglones;
+	}
+
+
+	public void setCantidadRenglones(Integer cantidadRenglones) {
+		this.cantidadRenglones = cantidadRenglones;
+	}
+
+
+	public String getPosicionPorcentual() {
+		return posicionPorcentual;
+	}
+
+
+	public void setPosicionPorcentual(String posicionPorcentual) {
+		this.posicionPorcentual = posicionPorcentual;
+	}
+
+
+	public String getAgrupamiento() {
+		return agrupamiento;
+	}
+
+
+	public void setAgrupamiento(String agrupamiento) {
+		this.agrupamiento = agrupamiento;
+	}
+
+
+	public String getCodigoEspecial() {
+		return codigoEspecial;
+	}
+
+
+	public void setCodigoEspecial(String codigoEspecial) {
+		this.codigoEspecial = codigoEspecial;
+	}
+
+
+	public String getCuitProveedor() {
+		return cuitProveedor;
+	}
+
+
+	public void setCuitProveedor(String cuitProveedor) {
+		this.cuitProveedor = cuitProveedor;
+	}
+
+
+	public Date getFechaLiquidacion() {
+		return fechaLiquidacion;
+	}
+
+
+	public void setFechaLiquidacion(Date fechaLiquidacion) {
+		this.fechaLiquidacion = fechaLiquidacion;
+	}
+
+
+	public String getEstadoDadoDeBaja() {
+		return estadoDadoDeBaja;
+	}
+
+
+	public void setEstadoDadoDeBaja(String estadoDadoDeBaja) {
+		this.estadoDadoDeBaja = estadoDadoDeBaja;
+	}
+
+
+	public String getMenuOrigenDatos() {
+		return menuOrigenDatos;
+	}
+
+
+	public void setMenuOrigenDatos(String menuOrigenDatos) {
+		this.menuOrigenDatos = menuOrigenDatos;
+	}
+
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+
+	public String getUserSequentialCreacion() {
+		return userSequentialCreacion;
+	}
+
+
+	public void setUserSequentialCreacion(String userSequentialCreacion) {
+		this.userSequentialCreacion = userSequentialCreacion;
+	}
+
+
+	public Date getFechaUltimaModificacion() {
+		return fechaUltimaModificacion;
+	}
+
+
+	public void setFechaUltimaModificacion(Date fechaUltimaModificacion) {
+		this.fechaUltimaModificacion = fechaUltimaModificacion;
+	}
+
+
+	public String getUserSequentialUltimaModificacion() {
+		return userSequentialUltimaModificacion;
+	}
+
+
+	public void setUserSequentialUltimaModificacion(
+			String userSequentialUltimaModificacion) {
+		this.userSequentialUltimaModificacion = userSequentialUltimaModificacion;
+	}
+
+
+	public Date getFechaOdp() {
+		return fechaOdp;
+	}
+
+
+	public void setFechaOdp(Date fechaOdp) {
+		this.fechaOdp = fechaOdp;
+	}
+
+
+	public Integer getNumeroOdp() {
+		return numeroOdp;
+	}
+
+
+	public void setNumeroOdp(Integer numeroOdp) {
+		this.numeroOdp = numeroOdp;
+	}
+
+
+	public Date getFechaCajaDiaria() {
+		return fechaCajaDiaria;
+	}
+
+
+	public void setFechaCajaDiaria(Date fechaCajaDiaria) {
+		this.fechaCajaDiaria = fechaCajaDiaria;
+	}
+
+
+	public Integer getNumeroCajaDiaria() {
+		return numeroCajaDiaria;
+	}
+
+
+	public void setNumeroCajaDiaria(Integer numeroCajaDiaria) {
+		this.numeroCajaDiaria = numeroCajaDiaria;
+	}
+
+
+	public String getIdGasto() {
+		return idGasto;
+	}
+
+
+	public void setIdGasto(String idGasto) {
+		this.idGasto = idGasto;
+	}
+
+
+	public boolean getIsReadMode() {
+		return isReadMode;
+	}
+
+
+	public void setIsReadMode(boolean isReadMode) {
+		this.isReadMode = isReadMode;
+	}
+
+
+	public String getLockedBy() {
+		return lockedBy;
+	}
+
+
+	public void setLockedBy(String lockedBy) {
+		this.lockedBy = lockedBy;
+	}
+
+
+	public static String getCodigoRegistro() {
+		return codigoRegistro;
+	}
+	
 }
