4b5e923d837952f7d048815b7d9537a6c42342c0
Fernando <fernandor@applinet.com.ar>
2018-06-29 15:57:21 -0300

    Barra de opciones de gastos

---
+++ L8669/Code/Java/ar/com/ada3d/frontend/LayoutTitleBarActionsOpcionesGastos.java
@@ -0,0 +1,24 @@
+package ar.com.ada3d.frontend;
+
+import com.ibm.xsp.extlib.tree.impl.BasicLeafTreeNode;
+import com.ibm.xsp.extlib.tree.impl.BasicNodeList;
+
+public class LayoutTitleBarActionsOpcionesGastos extends BasicNodeList {
+private static final long serialVersionUID = 1L;
+
+
+	public LayoutTitleBarActionsOpcionesGastos() {
+		addLeaf("Cfg edificio nuevo", "btnNuevaOpcionGasto", "btn-info");
+	}
+	
+	public void addLeaf(String label, String boton, String styleClass) {
+		
+		BasicLeafTreeNode node = new BasicLeafTreeNode();
+		node.setLabel(label);
+		String script = "dojo.query(\"[id$='" + boton + "']\")[0].click();";
+	    node.setOnClick(script);
+	    node.setStyleClass(styleClass);
+		addChild(node);
+	}
+
+}
