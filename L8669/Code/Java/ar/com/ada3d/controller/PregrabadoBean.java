31a11890d6ed02840501870234f69386f0e9d3df
Fernando <fernandor@applinet.com.ar>
2018-10-04 11:45:23 -0300

    Incluyo el gasto en el pregrabado para los datos al final

--- L8669/Code/Java/ar/com/ada3d/controller/PregrabadoBean.java
+++ L8669/Code/Java/ar/com/ada3d/controller/PregrabadoBean.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ar.com.ada3d.model.Edificio;
+import ar.com.ada3d.model.Gasto;
 import ar.com.ada3d.model.Porcentual;
 import ar.com.ada3d.model.TextoPregrabado;
 import ar.com.ada3d.model.TextoPregrabadoEdificio;
@@ -12,21 +13,24 @@ import ar.com.ada3d.model.TextoPregrabadoEdificio;
 public class PregrabadoBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private TextoPregrabado textoPregrabado;
-
+	private Gasto myGasto;
 	
 	public PregrabadoBean() {
 		// Empty constructor
 	}
+
 	
-	
-	public void cargoPregrabado(List<String> prm_edificios) {
-		if(textoPregrabado == null)
+	public void initPregrabado(List<String> prm_edificios, Gasto prm_gasto) {
+		myGasto = prm_gasto;
+		if(textoPregrabado == null){
+			System.out.println("FPR new TextoPregrabado");
 			setTextoPregrabado(new TextoPregrabado());
-		textoPregrabado.setListaEdificios(prm_edificios);
-		textoPregrabado.setEdificios(cargaListaTextoPregrabadoEdificio(prm_edificios));	
+		}else{
+			System.out.println("FPR NO ES PRIMERA VEZ PUEDO NULEAR?=" );
 			
-		System.out.println("FPR seguir aca");	
-		
+		}
+		textoPregrabado.setListaEdificios(prm_edificios);
+		textoPregrabado.setEdificios(fillListaJsonEdificios(prm_edificios));	
 	}
 	
 	
@@ -34,19 +38,18 @@ public class PregrabadoBean implements Serializable {
 	 * @param prm_edificios
 	 * @return lista de TextoPregrabadoEdificio
 	 */
-	private List<TextoPregrabadoEdificio> cargaListaTextoPregrabadoEdificio(List<String> prm_edificios){
+	private List<TextoPregrabadoEdificio> fillListaJsonEdificios(List<String> prm_edificios){
+		System.out.println("FPR fillListaJsonEdificios:" + prm_edificios);
 		Edificio myEdificio = null;
 		List<TextoPregrabadoEdificio> ret = new ArrayList<TextoPregrabadoEdificio>();
 		for (String edif : prm_edificios){
 			
 			myEdificio = ar.com.ada3d.utilidades.JSFUtil.getCacheApp().getHmEdificios().get(edif);
-			ret.add(cargaTextoPregrabadoEdificio(myEdificio));
+			ret.add(fillJsonEdificio(myEdificio));
 			myEdificio = null;
 		}
 		return ret;
 	}
-	
-	
 	
 	
 	/**Cargo el texto pregrabado de un edificio recibiendo el objeto edificio, estoy presionando Continuar en la pantalla 
@@ -55,37 +58,51 @@ public class PregrabadoBean implements Serializable {
 	 * @param prm_edificio el objeto edificio que le voy a cargar los porcentuales
 	 * @return los datos particulares de los edificios, solo esto ya que el resto lo cargo en el avance de carga de datos
 	 */
-	private TextoPregrabadoEdificio cargaTextoPregrabadoEdificio(Edificio prm_edificio){
-		final TextoPregrabadoEdificio textoPregrabadoEdificio = new TextoPregrabadoEdificio();
-		textoPregrabadoEdificio.setEdif(prm_edificio.getEdf_codigo());
+	private TextoPregrabadoEdificio fillJsonEdificio(Edificio prm_edificio){
+		TextoPregrabadoEdificio myTextoPregrabadoEdificio = new TextoPregrabadoEdificio();
+		myTextoPregrabadoEdificio.setEdif(prm_edificio.getEdf_codigo());
 		//TODO: URGENTE obtener el cuit de algun lado textoPregrabadoEdificio.setPrv_cuit(prv_cuit);
-		textoPregrabadoEdificio.setPorcentuales(cargaPorcentuales(prm_edificio));
-		return textoPregrabadoEdificio;
+		System.out.println("FPR agrupamiento de gasto=" + myGasto.getAgrupamiento() + " - edif=" + prm_edificio.getEdf_codigo());
+		myTextoPregrabadoEdificio.setAgrupamiento(myGasto.getAgrupamiento());
+		myTextoPregrabadoEdificio.setCodigoEspecial(myGasto.getCodigoEspecial());
+		myTextoPregrabadoEdificio.setPorcentuales(fillListaJsonPorcentuales(prm_edificio));
+		return myTextoPregrabadoEdificio;
 	}
 
 	
-	/**Devuelve lista de porcentuales para pregrabado
+	/**Devuelve lista de porcentuales para pregrabado seteando el tilde para ingreso de importe
 	 * 
 	 * @param prm_edificio el objeto edificio que le voy a cargar los porcentuales
 	 * @return lista de porcentuales a aplicar al pregrabadoEdificio
 	 */
-	private List<Porcentual> cargaPorcentuales(Edificio prm_edificio){
+	private List<Porcentual> fillListaJsonPorcentuales(Edificio prm_edificio){
 		List<Porcentual> listaPorcentuales = prm_edificio.getListaPorcentuales();
+		for(Porcentual por : listaPorcentuales){
+			if(!por.getPorc_porcentaje().equals(0))
+				por.setPorc_CheckBoxImporteJson(true);
+		}
 		return listaPorcentuales;
 	}
 	
+
+	
 	
 	//Getters & Setters
-	
-	
+		
 	public TextoPregrabado getTextoPregrabado() {
 		return textoPregrabado;
 	}
 
-
 	public void setTextoPregrabado(TextoPregrabado textoPregrabado) {
 		this.textoPregrabado = textoPregrabado;
-		
+	}
+
+	public Gasto getMyGasto() {
+		return myGasto;
+	}
+
+	public void setMyGasto(Gasto myGasto) {
+		this.myGasto = myGasto;
 	}
 	
 }
